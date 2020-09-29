8f164dc298439d558552ae1924bba3e080da6f8b
AnneLindberg <aans@live.dk>
2020-09-29 10:01:08 +0200

    Initial commit

---
+++ src/Client/ClientImpl.java
@@ -0,0 +1,5 @@
+package Client;
+
+public class ClientImpl implements Client
+{
+}
---
+++ src/Model/Coffeepot.java
@@ -0,0 +1,23 @@
+package Model;
+
+public class Coffeepot
+{
+  private String color;
+  private double capacity;
+
+  public Coffeepot(String color, double capacity)
+  {
+    this.color = color;
+    this.capacity = capacity;
+  }
+
+  public String getColor()
+  {
+    return color;
+  }
+
+  public double getCapacity()
+  {
+    return capacity;
+  }
+}
